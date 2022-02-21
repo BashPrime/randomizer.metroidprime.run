236fe97e2ea57bab7d0b517578c4de381b04c583
BashPrime <5769306+BashPrime@users.noreply.github.com>
2022-02-20 22:57:33 -0800

    Update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,18 @@
+import { Footer as FooterElem, BrandText } from './styles';
+
 export const Footer = () => {
     const currentYear = (new Date()).getFullYear();
 
     return (
-        <footer className="footer">
+        <FooterElem className="footer">
             <div className="content has-text-centered">
+                <BrandText>Metroid Prime Randomizer</BrandText>
                 <p>
                     The Metroid Prime series, in-game imagery, and system logos Copyright © 2002-{currentYear} Nintendo Co., Ltd.
                 </p>
                 <p><a href="https://metroidprime.run" target="_blank">metroidprime.run</a> does <strong>not</strong> distribute copyrighted material.</p>
             </div>
-        </footer>
+        </FooterElem>
     );
 };
 
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+    padding: 2rem 1.5rem;
+`;
+
+export const BrandText = styled.p`
+    font-weight: 700;
+    font-size: 2em;
+`;
